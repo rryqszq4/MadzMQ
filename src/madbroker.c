c22c613ea7eb0a72f07ff2e10555bf7d620f26e9
rryqszq4 <279664262@163.com>
2015-09-16 11:49:13 +0800

    update mad broker file

--- src/madbroker.c
+++ src/madbroker.c
@@ -49,5 +49,112 @@ main (void)
 	return 0;
 }
 
+typedef struct {
+	void *socket;
+	zframe_t *identity;
+	char *subtree;
+} kvroute_t;
+
+static int
+m_send_single(const char *key, void *data, void *args)
+{
+	kvroute_t *kvroute = (kvroute_t *)args;
+	kvmsg_t kvmsg = (kvmsg_t *)data;
+	if (strlen(kvroute->subtree) <= strlen(kvmsg_key(kvmsg)) 
+		&& memcmp(kvroute->subtree, kvmsg_key(kvmsg), strlen(kvroute->subtree)) == 0){
+		zframe_send(&kvroute->identity, kvroute->socket, ZFRAME_MORE + ZFRAME_REUSE);
+		kvmsg_send(kvmsg, kvroute->socket);
+	}
+	return 0;
+}
+
+static int
+m_snapshots(zloop_t *loop, zmq_pollitem_t *poller, void *args)
+{
+	mad_broker_t *self = (mad_broker_t *)args;
+
+	zframe_t *identity = zframe_recv(poller->socket);
+	if (identity){
+		char *request = zstr_recv(poller->socket);
+		char *subtree = NULL;
+		if (streq(request, "ICANHAZ?")){
+			free(request);
+			subtree = zstr_recv(poller->socket);
+		}else {
+			printf("E: bad request, aborting\n");
+		}
+
+		if (subtree){
+			kvroute_t routing = {poller->socket, identity, subtree};
+			zhash_foreach(self->kvmap, m_send_single, &routing);
+
+			zclock_log("I: sending snapshot=%d", (int)self->sequence);
+			zframe_send(&identity, poller->socket, ZFRAME_MORE);
+			kvmsg_t *kvmsg = kvmsg_new(self->sequence);
+			kvmsg_set_key(kvmsg, "KTHXBAI");
+			kvmsg_set_body(kvmsg, (byte *)subtree, 0);
+			kvmsg_send(kvmsg, poller->socket);
+			kvmsg_destroy(&kvmsg);
+			free(subtree);
+		}
+		zframe_destroy(&identity);
+	}
+	return 0;
+}
+
+static int 
+m_collector(zloop_t *loop, zmq_pollitem_t *poller, void *args)
+{
+	mad_broker_t *self = (mad_broker_t *)args;
+	if (kvmsg){
+		kvmsg_set_sequence(kvmsg, ++self->sequence);
+		kvmsg_send(kvmsg, self->publisher);
+		int ttl = atoi(kvmsg_get_prop(kvmsg, "ttl"));
+		if (ttl)
+			kvmsg_set_prop(kvmsg, "ttl", "%" PRID64, zclock_time() + ttl * 1000);
+		kvmsg_store(&kvmsg, self->kvmap);
+		zclock_log("I: publishing update=%d", (int)self->sequence);
+	}
+	return 0;
+}
+
+static int 
+m_flush_single(const char *key, void *data, void *args)
+{
+	mad_broker_t *self = (mad_broker_t *)args;
+
+	kvmsg_t *kvmsg = (kvmsg_t *)data;
+	int64_t ttl;
+	sscanf(kvmsg_get_prop(kvmsg, "ttl"), "%" PRID64, &ttl);
+	if (ttl && zclock_time() >= ttl){
+		kvmsg_set_sequence(kvmsg, ++self->sequence);
+		kvmsg_set_body(kvmsg, (byte *) "", 0);
+		kvmsg_send(kvmsg, self->publisher);
+		kvmsg_store(&kvmsg, self->kvmap);
+		zclock_log("I: publishing delete=%d", (int)self->sequence);
+	}
+	return 0;
+}
+
+static int
+m_flush_ttl(zloop_t *loop, int timer_id, void *args)
+{
+	mad_broker_t * self = (mad_broker_t *) args;
+	if (self->kvmap)
+		zhash_foreach(self->kvmap, m_flush_single, args);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
