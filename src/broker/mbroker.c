25b0a9b44b3bc6e7bc05d74b7f6691e6507ca497
rryqszq4 <279664262@163.com>
2015-09-24 12:59:50 +0800

    [broker] fix mbroker api

--- src/broker/mbroker.c
+++ src/broker/mbroker.c
@@ -1,28 +1,40 @@
-#include "mbroker.h"
 
-struct _mbroker_t
-{
-	zctx_t *ctx;
-	mbroute_t *client_route;
-	mbqueue_t *queue;
-	mbpublish_t *publish;
-	void *snapshot;
-	mbroute_t *service_route;
-	void *service;
-	mbstats_t *stats;
-	zloop_t *loop;
-};
+#include "mbroker.h"
 
 mbroker_t *
 mbroker_new()
 {
 	mbroker_t *this;
-	this = (mbroker_t) zmalloc(sizeof(mbroker_t));
+	this = (mbroker_t *) zmalloc(sizeof(mbroker_t));
 
+	this->verbose = true;
 	this->ctx = zctx_new();
 	this->loop = zloop_new();
+	this->client_route = mbroute_new(this->ctx, MBROUTE_CLIENT_HOST, MBROUTE_CLIENT_PORT);
+	//mbroute_bind(this->client_route);
+	zsocket_bind(this->client_route, "tcp://127.0.0.1:5555");
 
 	return this;
+}
+
+void
+mbroker_loop_poller(mbroker_t *this, zloop_fn handler)
+{
+	zmq_pollitem_t poller = { 0, 0, ZMQ_POLLIN};
+	poller.socket = this->client_route->socket;
+	zloop_poller(this->loop, &poller, handler, this);
+}
+
+void 
+mbroker_loop_timer(mbroker_t *this, size_t delay, size_t time,  zloop_timer_fn handler)
+{
+	zloop_timer(this->loop, delay, time, handler, this);
+}
+
+void
+mbroker_looper(mbroker_t *this)
+{
+	zloop_start(this->loop);
 }
 
 void
@@ -31,10 +43,22 @@ mbroker_destroy(mbroker_t **this_p)
 	assert(this_p);
 	if (*this_p){
 		mbroker_t *this = *this_p;
+		mbroute_destroy(this->client_route);
 		zloop_destroy(&this->loop);
 		zctx_destroy(&this->ctx);
 		free(this);
 		*this_p = NULL;
 	}
+}
+
+int
+client_route_recv_handle(mbroker_t *this)
+{
+	printf("123\n");
+	zmsg_t *msg = mbroute_recv(this->client_route);
+
+	zmsg_dump(msg);
+
+	return 0;
 }
 
--- src/broker/mbroker.h
+++ src/broker/mbroker.h
@@ -1,11 +1,45 @@
 #ifndef _MBROKER_H_INCLUDED_
 #define _MBROKER_H_INCLUDED_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "czmq.h"
+#include "mbroute.h"
+#include "mbqueue.h"
 
 typedef struct _mbroker_t mbroker_t;
 
+struct _mbroker_t
+{
+	zctx_t *ctx;
+	mbroute_t *client_route;
+	mbqueue_t *queue;
+	//mbpublish_t *publish;
+	void *snapshot;
+	mbroute_t *service_route;
+	void *service;
+	//mbstats_t *stats;
+	zloop_t *loop;
+	bool verbose;
+};
+
+#define MBROKER_HOST 			"127.0.0.1"
+#define MBROUTE_CLIENT_HOST		"127.0.0.1"
+#define MBROUTE_CLIENT_PORT		5555
+
 mbroker_t *mbroker_new();
 void mbroker_destroy(mbroker_t **this_p);
+
+void mbroker_loop_poller(mbroker_t *this, zloop_fn handler);
+void mbroker_loop_timer(mbroker_t *this, size_t delay, size_t time,  zloop_timer_fn handler);
+void mbroker_looper(mbroker_t *this);
+
+int client_route_recv_handle(mbroker_t *this);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
